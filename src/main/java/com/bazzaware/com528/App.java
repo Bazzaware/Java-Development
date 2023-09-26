d1f45de43fa41c869a87789427c535dde87ebac2
Bazza <barry.goodall@os.uk>
2023-09-26 15:03:04 +0100

    Added Student Class with tests and prints 5 new students

---
+++ src/main/java/com/bazzaware/com528/App.java
@@ -0,0 +1,23 @@
+package com.bazzaware.com528;
+
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            String firstName = "John" + i;
+            String lastName = "Doe" + i;
+            Double mark = 1.0 + i;
+            Student student = new Student(firstName, lastName, mark);
+            students.add(student);
+        }
+
+        for (Student student : students) {
+            System.out.println(student.ToString());
+        }
+    }
+}
