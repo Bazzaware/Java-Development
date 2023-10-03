01e576a4918eff514c843e88aef81cbeee82e4dd
Bazza <barry.goodall@os.uk>
2023-10-03 07:53:00 +0100

    Added and example for testing user input

--- student/src/test/java/com/bazzaware/HelperTests.java
+++ student/src/test/java/com/bazzaware/HelperTests.java
@@ -1,5 +1,26 @@
 package com.bazzaware;
 
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+
 public class HelperTests {
+    
+    @Test
+    public void ShouldPass(){
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream("3\n4\n".getBytes()));
+
+        ByteArrayOutputStream ByteArrayOutputStream = new ByteArrayOutputStream;
+        PrintStream printStream = new PrintStream(ByteArrayOutputStream);
+        PrintStream stdOut = System.out;
+        System.setOut(printStream);
+
+
+    }
+
 
 }
