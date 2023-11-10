8624182b0962bd62415e85be4b19f2bf201cde64
Bazza <barry@bazzaware.co.uk>
2023-11-10 20:01:40 +0000

    Add options for Comparator in TestTree.java

--- headfirst/src/main/java/com/headfirst/genetics/TestTree.java
+++ headfirst/src/main/java/com/headfirst/genetics/TestTree.java
@@ -14,6 +14,20 @@ public class TestTree {
         Book b3 = new Book("Finding Emo");
         /*
          * TreeSet elements must be Comparable. The Book class implements Comparable.
+         * Otherwise, we would get a ClassCastException.
+         * Another option would be to pass a Comparator to the TreeSet constructor.
+         * TreeSet<Book> tree = new TreeSet<>(new Comparator<Book>() {
+         * 
+         * @Override
+         * public int compare(Book o1, Book o2) {
+         * return o1.getTitle().compareTo(o2.getTitle());
+         * }
+         * });
+         * Or, using a lambda expression:
+         * TreeSet<Book> tree = new TreeSet<>((o1, o2) ->
+         * o1.getTitle().compareTo(o2.getTitle()));
+         * Or, using a method reference:
+         * TreeSet<Book> tree = new TreeSet<>(Comparator.comparing(Book::getTitle));
          */
         Set<Book> tree = new TreeSet<>();
         tree.add(b1);
